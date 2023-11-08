30914f2ccc4f2654a76e4153da3cd9ded9fa5df6
Mateus <136277117+MateusVolek@users.noreply.github.com>
2023-11-07 22:51:55 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao sistema de dados")
 //Mais um colsole log de teste
 console.log("O Mateus fez esse commit sen você ver na aula!")
+console.log("Enviando para o GitHub")

2eb5943675b4e1183c4be23e22f497d860e37eab
Mateus <136277117+MateusVolek@users.noreply.github.com>
2023-11-07 22:47:05 -0300

    console log indicando modificaçoes do Mateus

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao sistema de dados")
+//Mais um colsole log de teste
+console.log("O Mateus fez esse commit sen você ver na aula!")
